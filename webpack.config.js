3197d9354c7f8bdeb744a90eeec52410ae75784d
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-07-26 17:21:17 -0400

    Add html-webpack to config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index',
@@ -6,4 +7,10 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Restaurant Page',
+            template: './src/index.html'
+        })
+    ]
 };
