c138f9ef43700467b192bc06056ac1f76cc5fa0c
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-07-27 00:34:15 -0400

    Add tab switching between menu and home

--- src/index.js
+++ src/index.js
@@ -2,7 +2,27 @@ import './style.css';
 import loadHome from './home.js';
 import loadMenu from './menu.js';
 
-//loadHome();
-loadMenu();
+loadHome();
+
+const content = document.querySelector("#content");
+
+const homeBtn = document.querySelector("#home");
+const menuBtn = document.querySelector("#menu");
+
+function clearContent() {
+    while (content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
+}
+
+menuBtn.addEventListener('click', () => {
+    clearContent();
+    loadMenu();
+});
+
+homeBtn.addEventListener('click', () => {
+    clearContent();
+    loadHome();
+})
 
 //alert("Everything's bundled correctly! Cheers to that.");
