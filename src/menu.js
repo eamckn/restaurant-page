229a7c11bd062597f171da3a382ad34c42867de1
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-07-27 00:22:34 -0400

    Add item name below images in menu module

--- src/menu.js
+++ src/menu.js
@@ -37,31 +37,49 @@ export default function loadMenu() {
     misoItem.className = "menu-item";
     misoItem.setAttribute("id", "miso");
     misoItem.appendChild(misoImage);
+    const misoText = document.createElement("p");
+    misoText.innerHTML = "Miso Ramen";
+    misoItem.appendChild(misoText);
 
     const shioItem = document.createElement("div");
     shioItem.className = "menu-item";
     shioItem.setAttribute("id", "shio");
     shioItem.appendChild(shioImage);
+    const shioText = document.createElement("p");
+    shioText.innerHTML = "Shio Ramen";
+    shioItem.appendChild(shioText);
 
     const shoyuItem = document.createElement("div");
     shoyuItem.className = "menu-item";
     shoyuItem.setAttribute("id", "shoyu");
     shoyuItem.appendChild(shoyuImage);
+    const shoyuText = document.createElement("p");
+    shoyuText.innerHTML = "Shoyu Ramen";
+    shoyuItem.appendChild(shoyuText);
 
     const kimchiItem = document.createElement("div");
     kimchiItem.className = "menu-item";
     kimchiItem.setAttribute("id", "kimchi");
     kimchiItem.appendChild(kimchiImage);
+    const kimchiText = document.createElement("p");
+    kimchiText.innerHTML = "Kimchi Ramen";
+    kimchiItem.appendChild(kimchiText);
 
     const tsukemenItem = document.createElement("div");
     tsukemenItem.className = "menu-item";
     tsukemenItem.setAttribute("id", "tsukemen");
     tsukemenItem.appendChild(tsukemenImage);
+    const tsukemenText = document.createElement("p");
+    tsukemenText.innerHTML = "Tsukemen Ramen";
+    tsukemenItem.appendChild(tsukemenText);
 
     const specialItem = document.createElement("div");
     specialItem.className = "menu-item";
     specialItem.setAttribute("id", "special");
     specialItem.appendChild(specialImage);
+    const specialText = document.createElement("p");
+    specialText.innerHTML = "Chef's Special Ramen";
+    specialItem.appendChild(specialText);
 
     let menu = [misoItem, shioItem, shoyuItem, kimchiItem, tsukemenItem, specialItem];
 
